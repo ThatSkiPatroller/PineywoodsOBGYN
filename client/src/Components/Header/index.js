b8ac23fac5b3252609f1a76b8d45e74c3e32b921
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-08-24 16:42:04 -0500

    Removed styling that was causing errors

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -3,24 +3,25 @@ import Logo1 from '../../Images/Pineywoods-OBGYN-office-Logo.jpg';
 import Logo2 from '../../Images/Pineywoods-OBGYN-Sketched-Logo.jpg';
 import './style.css';
 
+
 function Header () {
     return(
         <div>
-            <header class="navbar navbar-expand navbar-light flex-column flex-md-row bd-navbar">
-                <a class="navbar-brand mr-0 mr-md-2" aria-label="Bootstrap"><img src={Logo1} width='100%' height='100%'/><title>Bootstrap</title></a>
+            <header class="navbar navbar-expand navbar-light flex-column bd-navbar">
+                <a class="navbar-brand mr-0 mr-md-2" aria-label="Bootstrap"><img src={Logo1} width='100%' height='100%' /><title>Bootstrap</title></a>
                 <div class="navbar-nav-scroll">
                     <ul class="navbar-nav bd-navbar-nav flex-row">
                         <li class="nav-item">
-                            <a type='button' class="nav-link active" href="/">Home</a>
+                            <a id='grey' class="nav-link active" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="/AboutUs">About Us</a>
+                            <a id='grey' class="nav-link active" href="/AboutUs">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="/Gallery">Gallery</a>
+                            <a id='grey' class="nav-link active" href="/Gallery">Gallery</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="/Updates">Updates</a>
+                            <a id='grey' class="nav-link active" href="/Updates">Updates</a>
                         </li>
                     </ul>
                 </div>
