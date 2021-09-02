50d173a9b184453ee2e0b755f2caf316e48f0ed7
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-09-02 00:58:55 -0500

    Ran build which added build folder

---
+++ client/build/precache-manifest.892d8f7b9aae8577e2f3b577685efd4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec757d3660f09c5fef68",
+    "url": "/static/css/main.85407731.chunk.css"
+  },
+  {
+    "revision": "ec757d3660f09c5fef68",
+    "url": "/static/js/main.2e403003.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a612ea3252c29a1cc617",
+    "url": "/static/js/2.c22fbf6a.chunk.js"
+  },
+  {
+    "revision": "10d962609194e37e0495c967d9675187",
+    "url": "/static/media/Pineywoods-OBGYN-office-Logo.10d96260.jpg"
+  },
+  {
+    "revision": "421d67e53ad2814dbf94b60884afc854",
+    "url": "/static/media/Pineywoods-OBGYN-Sketched-Logo.421d67e5.jpg"
+  },
+  {
+    "revision": "05a97e08e4476182e76f6202c5269f7c",
+    "url": "/index.html"
+  }
+];
