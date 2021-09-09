f37fac009f8be3ec743f6c9c87408be20883034a
Trace Suiter <t.suit@utexas.edu>
2021-09-09 15:44:36 -0500

    Added switch to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
-import { BrowserRouter as Router, Route } from  "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from  "react-router-dom";
 import Home from './Pages/Home';
 import AboutUs from './Pages/AboutUs';
 import Gallery from './Pages/Gallery';
@@ -11,10 +11,12 @@ function App () {
   return(
     <Router>
       <div>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/AboutUs' component={AboutUs} />
-        <Route exact path='/Gallery' component={Gallery} />
-        <Route exact path='/Updates' component={Updates} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/AboutUs' component={AboutUs} />
+          <Route exact path='/Gallery' component={Gallery} />
+          <Route exact path='/Updates' component={Updates} />
+        </Switch>
       </div>
     </Router>
   )
