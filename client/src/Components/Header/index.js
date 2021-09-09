30cfcc08e0c11be219cb7408157d2c99310ec2c2
Trace Suiter <t.suit@utexas.edu>
2021-09-09 04:24:00 -0500

    Removed bad imports and commented code

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import Logo1 from '../../Images/scaled-logo.png';
-import Button from '@material-ui/core/Button';
 import './style.css';
 
 
 function Header () {
     return(
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href='/'>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href='/'>
         <img src={Logo1}/>
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item" >
-          <a class="nav-link active"  aria-current="page" href="/">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item" >
+          <a className="nav-link active"  aria-current="page" href="/">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/AboutUs">About Us</a>
+        <li className="nav-item">
+          <a className="nav-link active" href="/AboutUs">About Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/Gallery">Gallery</a>
+        <li className="nav-item">
+          <a className="nav-link active" href="/Gallery">Gallery</a>
         </li> 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Services
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Laser Treatment</a></li>
-            <li><a class="dropdown-item" href="#">Ultrasound</a></li>
-            <li><a class="dropdown-item" href="#">Pumpkins</a></li>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Laser Treatment</a></li>
+            <li><a className="dropdown-item" href="#">Ultrasound</a></li>
+            <li><a className="dropdown-item" href="#">Pumpkins</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/Updates">Updates</a>
+        <li className="nav-item">
+          <a className="nav-link active" href="/Updates">Updates</a>
         </li>
        
       </ul>
@@ -47,25 +46,7 @@ function Header () {
         </div>
     )
 }
- {/* <header class="navbar navbar-expand navbar-light flex-column bd-navbar">
-                <a class="navbar-brand mr-0 mr-md-2" aria-label="Bootstrap"><img src={Logo1} width='100%' height='100%' /><title>Bootstrap</title></a>
-                <div class="navbar-nav-scroll">
-                    <ul class="navbar-nav bd-navbar-nav flex-row">
-                        <li class="nav-item">
-                            <a id='grey' class="nav-link active" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id='grey' class="nav-link active" href="/AboutUs">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id='grey' class="nav-link active" href="/Gallery">Gallery</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id='grey' class="nav-link active" href="/Updates">Updates</a>
-                        </li>
-                    </ul>
-                </div>
-            </header> */}
+
 export default Header;
 
 
