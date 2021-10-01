11615bcfe3466f73c79a05805d623d28b6e7f974
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-10-01 03:18:29 -0500

    Compressed and added photos to photo gallery, also added index.js for easier importing to modules

---
+++ client/src/BabyPics/index.js
@@ -0,0 +1,25 @@
+const BabyPics = {
+    a: require('./a.jpeg').default,
+    b: require('./b.jpeg').default,
+    c: require('./c.jpeg').default,
+    d: require('./d.jpg').default,
+    e: require('./e.jpg').default,
+    f: require('./f.jpeg').default,
+    g: require('./g.jpeg').default,
+    h: require('./h.jpeg').default,
+    i: require('./i.jpg').default,
+    j: require('./j.jpg').default,
+    k: require('./k.jpg').default,
+    l: require('./l.jpg').default,
+    m: require('./m.jpeg').default,
+    n: require('./n.jpeg').default,
+    o: require('./o.jpg').default,
+    p: require('./p.jpg').default,
+    q: require('./q.jpg').default,
+    r: require('./r.jpg').default,
+    s: require('./s.jpg').default,
+    t: require('./t.jpg').default,
+    u: require('./u.jpeg').default
+}
+
+export default BabyPics;
--- client/src/Components/Gal/index.js
+++ client/src/Components/Gal/index.js
@@ -1,32 +1,124 @@
+import { keys } from '@material-ui/core/styles/createBreakpoints';
 import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
-import a from '../../BabyPics/a.jpeg';
-import b from '../../BabyPics/b.jpeg';
-import c from '../../BabyPics/c.jpeg'; 
-import d from '../../BabyPics/d.jpg';
-import e from '../../BabyPics/e.jpg';
+import BabyPics from '../../BabyPics';
+// import a from '../../BabyPics/a.jpeg';
+// import b from '../../BabyPics/b.jpeg';
+// import c from '../../BabyPics/c.jpeg'; 
+// import d from '../../BabyPics/d.jpg';
+// import e from '../../BabyPics/e.jpg';
+// import f from '../../BabyPics/f.jpeg';
+// import g from '../../BabyPics/g.jpeg';
+// import h from '../../BabyPics/h.jpeg';
+// import i from '../../BabyPics/i.jpg';
+// import j from '../../BabyPics/'
 import './style.css';
 
 const images = [
     {
-        original: a,
+        original: BabyPics.a,
+        thumbnail: BabyPics.a,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
-        original: b,
+        original: BabyPics.b,
+        thumbnail: BabyPics.b,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
-        original: c,
+        original: BabyPics.c,
+        thumbnail: BabyPics.c,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
-        original: d,
+        original: BabyPics.d,
+        thumbnail: BabyPics.d,
         description: ''
     },
     {
-        original: e,
+        original: BabyPics.e,
+        thumbnail: BabyPics.e,
         description: 'Bryson'
+    },
+    {
+        original: BabyPics.f,
+        thumbnail: BabyPics.f,
+        description: 'Lucy Kate'
+    },
+    {
+        original: BabyPics.g,
+        thumbnail: BabyPics.g,
+        description: 'Lucy Kate'
+    },
+    {
+        original: BabyPics.h,
+        thumbnail: BabyPics.h,
+        description: 'Lucy Kate'
+    },
+    {
+        original: BabyPics.i,
+        thumbnail: BabyPics.i,
+        description: 'Emma Kate Barnett'
+    },
+    {
+        original: BabyPics.j,
+        thumbnail: BabyPics.j,
+        description: 'Holden Asher Rosencranz, DOB 8/17/2021'
+    },
+    {
+        original: BabyPics.k,
+        thumbnail: BabyPics.k,
+        description: 'Holden Asher Rosencranz, DOB 8/17/2021'
+    },
+    {
+        original: BabyPics.l,
+        thumbnail: BabyPics.l,
+        description: 'Holden Asher Rosencranz, DOB 8/17/2021'
+    },
+    {
+        original: BabyPics.m,
+        thumbnail: BabyPics.m,
+        description: 'Emory Rena Murphy'
+    },
+    {
+        original: BabyPics.n,
+        thumbnail: BabyPics.n,
+        description: 'Emory Rena Murphy'
+    },
+    {
+        original: BabyPics.o,
+        thumbnail: BabyPics.o,
+        description: 'Avryn Edwards'
+    },
+    {
+        original: BabyPics.p,
+        thumbnail: BabyPics.p,
+        description: 'Avryn Edwards'
+    },
+    {
+        original: BabyPics.q,
+        thumbnail: BabyPics.q,
+        description: ''
+    },
+    {
+        original: BabyPics.r,
+        thumbnail: BabyPics.r,
+        description: 'Samuel Juan'
+    },
+    {
+        original: BabyPics.s,
+        thumbnail: BabyPics.s,
+        description: 'Samuel Juan'
+    },
+    {
+        original: BabyPics.t,
+        thumbnail: BabyPics.t,
+        description: 'Samuel Juan'
+    },
+    {
+        original: BabyPics.u,
+        thumbnail: BabyPics.u,
+        description: 'Anthony Erath'
     }
 ]
 
