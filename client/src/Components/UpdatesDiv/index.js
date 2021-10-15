8fb88d57700ec54263bb79eabe44717b898b43a5
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-10-15 01:50:28 -0500

    Added Google Maps component under Googmaps, still needs api key

---
+++ client/src/Components/UpdatesDiv/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+function UpdatesDiv () {
+    return(
+        <div className='jack'>
+            
+        </div>
+    )
+}
+
+export default UpdatesDiv;
--- client/src/Pages/Updates.js
+++ client/src/Pages/Updates.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Headings from '../Components/Headings';
+import UpdatesDiv from '../Components/UpdatesDiv';
 
 function Updates () {
     return (
         <div>
             <Header />
             <Headings header='Updates' />
+            <UpdatesDiv />
         </div>
     )
 }
