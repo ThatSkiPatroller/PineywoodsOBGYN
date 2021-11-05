0ba35152db34b953fc2a6c633bbdcfa73b83c099
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-05 03:15:38 -0500

    Revert "Added Hours, phone #, address to home page on a new card with Google maps, added Dr. Suiter's bio and fixed padding"
    This reverts commit 5f3ebc39cf38b7620fca00dedc65f67aeca31097.

--- client/src/Components/GoogMap/index.js
+++ client/src/Components/GoogMap/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { Card } from '@material-ui/core';
-import './style.css';
 
 function GoogMap () {
     
     const mapStyles = {
-        height: '30vh',
-        width: '25vh'
+        height: '100vh',
+        width: '100%'
     }
 
     const defaultCenter = {
@@ -15,17 +13,6 @@ function GoogMap () {
     }
 
     return (
-        <Card className='mike mx-auto'>
-            <div className='row'>
-            <div className='col-6 fred'>
-                <br></br>
-                <h1>Hours:</h1>
-                <p>Monday - Thusday: 8 AM - 5 PM </p>
-                <p>Friday: 8 AM - 12 PM </p>
-                <p><h1>Phone Number: </h1> 936-699-7575</p>
-                <p><h1>Address:</h1> 402 Gaslight Blvd, Lufkin, Tx</p>
-            </div>
-            <div className='col-6'>
         <LoadScript
         googleMapsApiKey='AIzaSyC9r_0ONzMoOyUYewrxMScWrz2xOgP1ZJ0'>
             <GoogleMap  
@@ -35,7 +22,6 @@ function GoogMap () {
                 />
                 <Marker position={{lat: defaultCenter.lat, lng: defaultCenter.lng}} />
         </LoadScript>
-        </div></div></Card>
     )
 }
 
--- client/src/Pages/AboutUs.js
+++ client/src/Pages/AboutUs.js
@@ -10,15 +10,9 @@ function AboutUs () {
         <div>
             <Header />
             <Headings header='Staff Members' />
-            <LeftProfile 
-                picture={ProfilePics.SuiterProfile} 
-                name='Dr. Cheryl Suiter, OBGYN' 
-                bio='Dr. Suiter grew up in Diboll, Texas. She married Wes Suiter in 1982. She graduated from Stephen F.Austin State University with a Bachelor’s degree in Nursing. During her undergraduate years she worked at Woodland Heights as a nurse’s aide and ward clerk. Once she graduated, she went on to work her way through medical school in labor and delivery. She knew from a very young age she wanted to be a doctor, however, knew she would need another career to get her there.
-                She graduated with honors ( in the top 10%)from University of Texas Health Science Center in San Antonio and was accepted to residency and fellowship there. She received and award residency for research and has be published. She left fellowship/Assistant Clinical Professor in San Antonio to come back home to be closer to her family.
-                She has been in practice since 1998 in Lufkin. She has mentored nurses, nurse practitioners, medical students, and ultrasound techs over the years. She loves to teach and loves ultrasound. She has recently authored an article “Be a Warrior, Not Just a Survivor “ after a journey with breast cancer. She has been voted Best of Lufkin for many years and was Healthcare Provider of the year for Angelina County in 2016.
-                Dr. Suiter practices the full scope of Obstetrics and Gynecology, including doing her own ultrasounds. She has been married to her husband Wes, since 1982. They have a son Trace whom has recently graduated from University of Texas.'
-            />
+            <LeftProfile picture={ProfilePics.Suiter} name='Dr. Cheryl Suiter, OBGYN' bio=''/>
             <LeftProfile picture={ProfilePics.Jana} name='Jana Brazil, R.N.'/>
+            <LeftProfile picture={ProfilePics.Vicki} />
         </div>
     )
 }
