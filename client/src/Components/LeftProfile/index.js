e7c2f21adde1e0b1d07010f2f49f10ae648447fc
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-05 09:40:08 -0500

    Changed div layout on rightProfile component and leftProfile component, added card. Text in bio still needs work

--- client/src/Components/LeftProfile/index.js
+++ client/src/Components/LeftProfile/index.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import { propTypes } from 'react-image-fade-in';
 import Jana from '../../ProfilePics/Jana.jpg'
+import { Card } from '@material-ui/core';
 import './style.css';
 
 function LeftProfile (props) {
     return(
-         <div>
-            <div className='col-12'>
-                 <div className='row'>
-                    <div className='col'>
-                        <p className='bob'><img src={props.picture} height='400px'/></p>
-                            <div className='row'>
-                                <div className='col'>
-                                    <div className='roony'>
-                                        <h1 ><b>{props.name}</b></h1>
-                                        <br />
-                                        <p>
-                                        {props.bio}
-                                        </p>
-                                    </div>
-                                </div> 
-                            </div>
-                        </div>
-                    </div>
+        //  <div>
+        //     <div className='col-12'>
+        //          <div className='row'>
+        //             <div className='col'>
+        //                 <p className='bob'><img src={props.picture} height='400px'/></p>
+        //                     <div className='row'>
+        //                         <div className='col'>
+        //                             <Card className='roony'>
+        //                                 <h1 ><b>{props.name}</b></h1>
+        //                                 <br />
+        //                                 <h1 className='coshey'>
+        //                                 {props.bio}
+        //                                 </h1>
+        //                             </Card>
+        //                         </div> 
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        
+        <div className='robot'>
+            <div className='row'>
+                <div className='col-4'>
+                    <p className='felix'><img src={props.picture} height='400px'/></p>
+                </div>
+                <div className='col-8'>
+                    <Card className='roony'>
+                        <h1><b>{props.name}</b></h1>
+                       
+                        <h1 className='coshey'>
+                            {props.bio}
+                        </h1>
+                    </Card>
                 </div>
             </div>
-        
+        </div>
     )
 }
 
--- client/src/Components/RightProfile/index.js
+++ client/src/Components/RightProfile/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import { Card } from '@material-ui/core'
 import './style.css';
 
 function RightProfile (props) {
     return(
-        <div>
-            <div className='col-12'>
-                 <div className='row'>
-                    <div className='col'>
-                        <p className='bryan'><img src={props.picture} height='400px'/></p>
-                            <div className='row'>
-                                <div className='col'>
-                                    <div className='luis'>
-                                        <h1 ><b>{props.name}</b></h1>
-                                        <br />
-                                        <p>
-                                        {props.bio}
-                                        </p>
-                                    </div>
-                                </div> 
-                            </div>
-                        </div>
-                    </div>
+        <div className='roomba'>
+            <div className='row'>
+                <div className='col-8'>
+                    <Card className='rooftop'>
+                        <h1><b>{props.name}</b></h1>
+                       
+                        <h1 className='ajay'>
+                            {props.bio}
+                        </h1>
+                    </Card>
+                </div>
+                <div className='col-4'>
+                    <p className='icecream'><img src={props.picture} height='400px'/></p>
                 </div>
             </div>
+        </div>
     )
 }
 
