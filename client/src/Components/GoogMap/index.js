500abc2b2d2d65f8d1186dc9ee02515b60f7a236
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-05 05:01:39 -0500

    Fixed responsiveness issue with GoogMap card, added styling

--- client/src/Components/GoogMap/index.js
+++ client/src/Components/GoogMap/index.js
@@ -6,7 +6,7 @@ import './style.css';
 function GoogMap () {
     
     const mapStyles = {
-        height: '30vh',
+        height: '25vh',
         width: '25vh'
     }
 
@@ -20,35 +20,38 @@ function GoogMap () {
         zoomControl: false,
         mapTypeControl: false,
         scaleControl: false,
-        streetViewControl: true,
+        streetViewControl: false,
         rotateControl: false,
-        fullscreenControl: true
+        fullscreenControl: true,
+        mapId: "33ad748ad4509de4"
     }
 
     return (
         <Card className='mike mx-auto'>
             <div className='row'>
-            <div className='col-6 fred'>
-                <br></br>
-                <h1>Hours:</h1>
-                <p>Monday - Thusday: 8 AM - 5 PM </p>
-                <p>Friday: 8 AM - 12 PM </p>
-                <h1>Phone Number: </h1> <p>936-699-7575</p>
-                <h1>Address:</h1> <p>402 Gaslight Blvd, Lufkin, Tx</p>
+                <div className='col-md-6 fred'>
+                    <br></br>
+                    <h1>Hours:</h1>
+                    <p>Monday - Thusday: 8 AM - 5 PM </p>
+                    <p>Friday: 8 AM - 12 PM </p>
+                    <h1>Phone Number: </h1> <p>936-699-7575</p>
+                    <h1>Address:</h1> <p>402 Gaslight Blvd, Lufkin, Tx</p>
+                </div>
+            <div className='col-md-6 steve'>
+                <LoadScript
+                googleMapsApiKey='AIzaSyC9r_0ONzMoOyUYewrxMScWrz2xOgP1ZJ0'>
+                    <GoogleMap  
+                        mapContainerStyle={mapStyles}
+                        zoom={14}
+                        center={defaultCenter}
+                        options={ops}
+                    >
+                        <Marker position={defaultCenter} label={label}/>
+                    </GoogleMap>
+                </LoadScript>
             </div>
-            <div className='col-6'>
-        <LoadScript
-        googleMapsApiKey='AIzaSyC9r_0ONzMoOyUYewrxMScWrz2xOgP1ZJ0'>
-            <GoogleMap  
-                mapContainerStyle={mapStyles}
-                zoom={13}
-                center={defaultCenter}
-                options={ops}
-                >
-                <Marker position={defaultCenter} label={label}/>
-                </GoogleMap>
-        </LoadScript>
-        </div></div></Card>
+        </div>
+        </Card>
     )
 }
 
