af2f0e2cad2fb3f1297ca90ef159d153f743ed0c
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-07 03:28:43 -0600

    Added temporary readme.md file

--- README.md
+++ README.md
@@ -1,29 +1,18 @@
-# Create React Express App
-
-## About This Boilerplate
-
-This setup allows for a Node/Express/React app which can be easily deployed to Heroku.
-
-The front-end React app will auto-reload as it's updated via webpack dev server, and the backend Express app will auto-reload independently with nodemon.
-
-## Starting the app locally
-
-Start by installing front and backend dependencies. While in this directory, run the following command:
-
-```
-npm install
-```
-
-This should install node modules within the server and the client folder.
-
-After both installations complete, run the following command in your terminal:
-
-```
-npm start
-```
-
-Your app should now be running on <http://localhost:3000>. The Express server should intercept any AJAX requests from the client.
-
-## Deployment (Heroku)
-
-To deploy, simply add and commit your changes, and push to Heroku. As is, the NPM scripts should take care of the rest.
+# Pineywoods OBGYN
+https://github.com/undefined/undefined
+# Description
+In-progress website for Pineywoods OBGYN. Currently using React with express server hosted on netlify. Server is running but hashrouter being implemented instead.    
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Questions](#questions)
+# Installation
+To install the necessary dependencies for this project run npm i
+# Usage
+To use this application run npm start
+# Contributing
+Contributors: @erichand18
+# Questions
+Please see my github: ThatSkiPatroller
+You can ask me questions here: t.suit@utexas.edu
--- client/src/Components/LaserBio/index.js
+++ client/src/Components/LaserBio/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ProfilePics from '../../ProfilePics';
+import { Card } from '@material-ui/core';
 import './style.css';
 
 function LaserBio () {
     return(
         <div>
-            <img src={ProfilePics.Laser} id='laser'/>
-            <p> Over the course of a lifetime, every woman feels the effects of childbirht and aging. 
-                With the loss of vaginal tone, lubrication, and elasticity, women can often benefit from CO2RE
-                Intima laser treatments. With the loss of elasticity and tone, women often suffer from stress urinary 
-                Incontinence (loss of urine with movement, cough or sneezing). We offer a non surgical and non painful 
-                treatment with CO2RE Intima Laser Treatment. CO2RE treatment is a safe and quick (10-15 minutes) non surgical
-                treatment without incisions. It works by delivering controlled (fractional) CO2 energy which stimulates the 
-                formation of collagen thus remodeling the tissue. Patient will recieve a treatment every 3 - 4 weeks for 3 
-                treatments and then a single treatment a year after. Please call us at 936-699-7575 for a consultation appointment.
-            </p>
+            <div className='row'>
+                <div className='col-4'>
+                    <img src={ProfilePics.Laser} className='laser'/>
+                </div>
+                <div className='col-8'>
+                    <Card className='jared'>
+                        <p> Over the course of a lifetime, every woman feels the effects of childbirht and aging. 
+                            With the loss of vaginal tone, lubrication, and elasticity, women can often benefit from CO2RE
+                            Intima laser treatments. With the loss of elasticity and tone, women often suffer from stress urinary 
+                            Incontinence (loss of urine with movement, cough or sneezing). We offer a non surgical and non painful 
+                            treatment with CO2RE Intima Laser Treatment. CO2RE treatment is a safe and quick (10-15 minutes) non surgical
+                            treatment without incisions. It works by delivering controlled (fractional) CO2 energy which stimulates the 
+                            formation of collagen thus remodeling the tissue. Patient will recieve a treatment every 3 - 4 weeks for 3 
+                            treatments and then a single treatment a year after. Please call us at 936-699-7575 for a consultation appointment.
+                        </p>
+                    </Card>
+                </div>
+            </div>
         </div>
     )
 }
