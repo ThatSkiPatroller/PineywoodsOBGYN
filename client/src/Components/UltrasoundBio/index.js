15a891610f4f1429b6f397c26eb5e267b7e9893e
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-10 14:53:02 -0600

    Added bio given by Dr. Suiter to Ultrasound page

--- client/src/Components/UltrasoundBio/index.js
+++ client/src/Components/UltrasoundBio/index.js
@@ -7,6 +7,20 @@ function UltrasoundBio () {
         <div>
             {/* <img src={ProfilePics.SuiterUltrasound} id='ultrasound'/> */}
             <div className='circleBase'></div>
+            <p>Dr. Suiter is accredited by the American Institute of Ultrasound
+                Medicine and Nuchal Translucency and Quality Review Program. She
+                performs her own ultrasound, as this is a part of her job she 
+                genuinely loves. Rather than waiting for a report days later, you 
+                will be able to receive Dr. Suiter's assessment during the ultrasound.
+            </p>
+            <p>
+                Dr. Suiter strives for good pictures and has 3D/4D capabilities. 
+                You will view the ultrasound on a large flat screen and can have 
+                pictures emailed to you. Dr. Suiter performs ultrasounds for her 
+                colleagues at request. If you are not a patient but would desire
+                an ultrasound, you can request an order for an ultrasound from 
+                your provider.
+            </p>
         </div>
     )
 }
