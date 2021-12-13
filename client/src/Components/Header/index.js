184b0ebc23af8f9904b28b3b114fa15577c2ce17
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-12-13 00:28:53 -0600

    Changed logo in nav bar and added a max-width to hopefully make img clearer on mobile

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo1 from '../../Images/scaled-logo.webp';
+import Logo1 from '../../Images/transparent-logo.webp';
 import './style.css';
 
 // ms-auto = right align, mx-auto = center, ml = left align
@@ -9,7 +9,7 @@ function Header () {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href='/'>
-            <img src={Logo1} alt='Pineywoods OBGYN Logo'/>
+            <img src={Logo1} alt='Pineywoods OBGYN Logo' className='transpLogo'/>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
