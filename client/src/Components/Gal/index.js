cce620b8a8369d9b00beaa752b0e6a3582361c27
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-12-13 00:12:02 -0600

    Set all pictures in gallery to be a certain height so there is no page jumping, changed headings font styling with text-shadow

--- client/src/Components/Gal/index.js
+++ client/src/Components/Gal/index.js
@@ -7,106 +7,127 @@ import './style.css';
 const images = [
     {
         original: BabyPics.a,
+        originalHeight: '600px',
         thumbnail: BabyPics.a,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
         original: BabyPics.b,
+        originalHeight: '600px',
         thumbnail: BabyPics.b,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
         original: BabyPics.c,
+        originalHeight: '600px',
         thumbnail: BabyPics.c,
         description: 'Levi Caycoya DOB: 8/3/21'
     },
     {
         original: BabyPics.d,
+        originalHeight: '600px',
         thumbnail: BabyPics.d,
         description: ''
     },
     {
         original: BabyPics.e,
+        originalHeight: '600px',
         thumbnail: BabyPics.e,
         description: 'Bryson'
     },
     {
         original: BabyPics.f,
+        originalHeight: '600px',
         thumbnail: BabyPics.f,
         description: 'Lucy Kate'
     },
     {
         original: BabyPics.g,
+        originalHeight: '600px',
         thumbnail: BabyPics.g,
         description: 'Lucy Kate'
     },
     {
         original: BabyPics.h,
+        originalHeight: '600px',
         thumbnail: BabyPics.h,
         description: 'Lucy Kate'
     },
     {
         original: BabyPics.i,
+        originalHeight: '600px',
         thumbnail: BabyPics.i,
         description: 'Emma Kate Barnett'
     },
     {
         original: BabyPics.j,
+        originalHeight: '600px',
         thumbnail: BabyPics.j,
         description: 'Holden Asher Rosencranz, DOB 8/17/2021'
     },
     {
         original: BabyPics.k,
+        originalHeight: '600px',
         thumbnail: BabyPics.k,
         description: 'Holden Asher Rosencranz, DOB 8/17/2021'
     },
     {
         original: BabyPics.l,
+        originalHeight: '600px',
         thumbnail: BabyPics.l,
         description: 'Holden Asher Rosencranz, DOB 8/17/2021'
     },
     {
         original: BabyPics.m,
+        originalHeight: '600px',
         thumbnail: BabyPics.m,
         description: 'Emory Rena Murphy'
     },
     {
         original: BabyPics.n,
+        originalHeight: '600px',
         thumbnail: BabyPics.n,
         description: 'Emory Rena Murphy'
     },
     {
         original: BabyPics.o,
+        originalHeight: '600px',
         thumbnail: BabyPics.o,
         description: 'Avryn Edwards'
     },
     {
         original: BabyPics.p,
+        originalHeight: '600px',
         thumbnail: BabyPics.p,
         description: 'Avryn Edwards'
     },
     {
         original: BabyPics.q,
+        originalHeight: '600px',
         thumbnail: BabyPics.q,
         description: ''
     },
     {
         original: BabyPics.r,
+        originalHeight: '600px',
         thumbnail: BabyPics.r,
         description: 'Samuel Juan'
     },
     {
         original: BabyPics.s,
+        originalHeight: '600px',
         thumbnail: BabyPics.s,
         description: 'Samuel Juan'
     },
     {
         original: BabyPics.t,
+        originalHeight: '600px',
         thumbnail: BabyPics.t,
         description: 'Samuel Juan'
     },
     {
         original: BabyPics.u,
+        originalHeight: '600px',
         thumbnail: BabyPics.u,
         description: 'Anthony Erath'
     }
