ca52da65336a40f88cf8a72ee67c7adeca18be5f
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-12-13 02:05:29 -0600

    Had a misspelling in swipeable

--- client/src/Components/Rousel/index.js
+++ client/src/Components/Rousel/index.js
@@ -24,7 +24,7 @@ function Rousel () {
           dynamicHeight={false}
           stopOnHover={false}
           animationHandler={'fade'}
-          swipable={false}
+          swipeable={false}
         >
           {/* <div>
             <img src={ProfilePics.SuiterProfile2} className='newYork' />
