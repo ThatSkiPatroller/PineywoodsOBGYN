cd1f6b5132329a00f15ebcf174b366f8a4057c3b
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-12-27 16:21:06 -0600

    Removed old updates div and added picture with clickable link to article on covid vax and pregnancy for updates tab

--- client/src/Components/UpdatesDiv/index.js
+++ client/src/Components/UpdatesDiv/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import BackgroundDiv from '../../Images/updateBG.png';
+import UpdateDivPic from '../../Images/UpdateDivPic.webp';
 import './style.css'
 
 function UpdatesDiv () {
     return(
-        // <div className='jack'>
-            
+        // <div className='containerZ'>
+        //     <div className='centeredText'>
+        //     Please bare with us as this website is still under development. 
+        //     If you have any comments or issues with the site, please contact Trace Suiter at t.suit@utexas.edu. 
+        //     You can also send your pictures to this email to be added to the gallery on this website.
+        //     In the subject of the email, please put 'Pineywoods Site Inquiry' to help me distinguish your email.
+        //     </div>
         // </div>
-        <div className='containerZ'>
-            {/* <img src={BackgroundDiv} className='nathaniel' /> */}
-            <div className='centeredText'>
-            Please bare with us as this website is still under development. 
-            If you have any comments or issues with the site, please contact Trace Suiter at t.suit@utexas.edu. 
-            You can also send your pictures to this email to be added to the gallery on this website.
-            In the subject of the email, please put 'Pineywoods Site Inquiry' to help me distinguish your email.
+        <div>
+            <a target='_blank' href='https://jamanetwork.com/journals/jama/fullarticle/2787240'>
+                <img src={UpdateDivPic} className='updateContainer' />
+            </a>
+            <div className='updateText'>
+                <h1>Click the image to view the full article</h1>
             </div>
         </div>
     )
