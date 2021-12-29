05d3e0689ee65ff6468ec1c4c7279cb4346e07f8
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-12-29 09:00:52 -0600

    Made more changes to updates page, added back the please bare with us div and more styling on updates page

--- client/src/Components/UpdatesDiv/index.js
+++ client/src/Components/UpdatesDiv/index.js
@@ -4,20 +4,23 @@ import './style.css'
 
 function UpdatesDiv () {
     return(
-        // <div className='containerZ'>
-        //     <div className='centeredText'>
-        //     Please bare with us as this website is still under development. 
-        //     If you have any comments or issues with the site, please contact Trace Suiter at t.suit@utexas.edu. 
-        //     You can also send your pictures to this email to be added to the gallery on this website.
-        //     In the subject of the email, please put 'Pineywoods Site Inquiry' to help me distinguish your email.
-        //     </div>
-        // </div>
         <div>
-            <a target='_blank' href='https://jamanetwork.com/journals/jama/fullarticle/2787240'>
-                <img src={UpdateDivPic} className='updateContainer' />
-            </a>
-            <div className='updateText'>
-                <h1>Click on image to view full article</h1>
+        
+            <div>
+                <a target='_blank' href='https://jamanetwork.com/journals/jama/fullarticle/2787240'>
+                    <img src={UpdateDivPic} className='updateContainer' />
+                </a>
+                <div className='updateText'>
+                    <h1>Click on image to view full article</h1>
+                </div>
+            </div>
+            <div className='containerZ'>
+                <div className='centeredText'>
+                Please bare with us as this website is still under development. 
+                If you have any comments or issues with the site, please contact Trace Suiter at t.suit@utexas.edu. 
+                You can also send your pictures to this email to be added to the gallery on this website.
+                In the subject of the email, please put 'Pineywoods Site Inquiry' to help me distinguish your email.
+                </div>
             </div>
         </div>
     )
