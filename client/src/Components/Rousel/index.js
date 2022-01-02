f238dc11db916d9a00489533342798c475c02191
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2022-01-01 23:54:15 -0600

    Fixed height difference problem on homepage while keeping CDN image src format

--- client/src/Components/Rousel/index.js
+++ client/src/Components/Rousel/index.js
@@ -25,19 +25,19 @@ function Rousel () {
             <img src='https://d1wedsacc4lfnu.cloudfront.net/SuiterSign.webp' className='carou' />
           </div>
           <div>
-            <img src='https://d1wedsacc4lfnu.cloudfront.net/Vicki.webp' className='carou'/>
+            <img src='https://d1wedsacc4lfnu.cloudfront.net/Vicki5.webp' className='carou'/>
           </div>
           <div>
             <img src='https://d1wedsacc4lfnu.cloudfront.net/JanaSign.webp' className='carou' />
           </div>
           <div>
-            <img src='https://d1wedsacc4lfnu.cloudfront.net/JanaBrendaKelli.webp' className='carou'/>
+            <img src='https://d1wedsacc4lfnu.cloudfront.net/JanaBrendaKelli6.webp' className='carou'/>
           </div>
           <div>
             <img src='https://d1wedsacc4lfnu.cloudfront.net/SandraSign.webp' className='carou' />
           </div>
           <div>
-            <img src='https://d1wedsacc4lfnu.cloudfront.net/Crystal.webp' className='carou' />
+            <img src='https://d1wedsacc4lfnu.cloudfront.net/Crystal&Cynthia.webp' className='carou' />
           </div>
         </Carousel>
         </div>
