b850e1c915016ece25d65cc0d728da635c5b706e
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2022-01-02 00:24:10 -0600

    Changed ids into classnames in Slide component bc text in still has bug. Seeing if this will fix it on hosted site

--- client/src/Components/Slide/index.js
+++ client/src/Components/Slide/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Slider from 'react-slick';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faStar } from '@fortawesome/free-solid-svg-icons';
 import FiveStars from '../FiveStars';
 import './style.css'
 
@@ -21,17 +19,17 @@ function Slide () {
      return(
         <div>
             <Slider {...settings}>
-                <div id='trace' align='center'>
+                <div className='slideText' align='center'>
                     <h3>"Dr. Suiter and her staff are wonderful! <br /> Our family is so thankful for them ❤️"</h3>
                     <FiveStars />
                     <p>Catherine B. <b>Facebook</b></p>
                 </div>
-                <div id='trace' align='center'>
+                <div className='slideText' align='center'>
                     <h3>"Dr. Suiter is hands down the best <br /> doctor with the most wonderful staff! 💙 <br /> Could not have chosen a better doctor to help me <br />bring my baby boy into the world! 🥰"</h3>
                     <FiveStars />
                     <p>Ana H. <b>Facebook</b></p>
                 </div>
-                <div id='trace' align='center'>
+                <div className='slideText' align='center'>
                     <h3>"Dr. Suiter is the absolute BEST!! <br />
                         She delivered both of my babies <br /> and my husband and I just love her and <br /> ALL of the staff at the office! ❤️"</h3>
                     <FiveStars />
