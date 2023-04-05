a5d557b73e33dde355c6ee20dad725525ec45c4a
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2023-04-05 11:39:19 -0500

    Fixing build error

--- client/src/Components/FadeInLogo/index.js
+++ client/src/Components/FadeInLogo/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ImageFadeIn from 'react-image-fade-in';
-import logo from '../../Images/transparent-logo.webp';
-import './style.css';
+// import React from 'react';
+// import ImageFadeIn from 'react-image-fade-in';
+// import logo from '../../Images/transparent-logo.webp';
+// import './style.css';
 
-function FadeInLogo () {
-    return(
-        <ImageFadeIn width={250} height={250} opacityTransition={6} className='pureLeaf' src={logo} srcset='logo 1.5x, logo 2x, logo 3x, logo 4x, logo 100x' sizes='70vmin' loadAsBackgroundImage={false}/>
-    )
-}
+// function FadeInLogo () {
+//     return(
+//         <ImageFadeIn width={250} height={250} opacityTransition={6} className='pureLeaf' src={logo} srcset='logo 1.5x, logo 2x, logo 3x, logo 4x, logo 100x' sizes='70vmin' loadAsBackgroundImage={false}/>
+//     )
+// }
 
-export default FadeInLogo
+// export default FadeInLogo
